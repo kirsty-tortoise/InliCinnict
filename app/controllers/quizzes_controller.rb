a1333bc68da8812704f7cfb1b78f3eebe4dd5665
kirsty-tortoise <kirstyland29@gmail.com>
2021-01-16 21:58:17 +0000

    routes for creating quizzes

--- app/controllers/quizzes_controller.rb
+++ app/controllers/quizzes_controller.rb
@@ -2,4 +2,24 @@ class QuizzesController < ApplicationController
     def index
       @quizzes = Quiz.all
     end
+
+    def new
+      @quiz = Quiz.new
+    end
+
+    def create
+      @quiz = Quiz.new(quiz_params)
+  
+      if @quiz.save
+        redirect_to quizzes_path
+      else
+        render :new
+      end
+    end
+  
+    private
+      def quiz_params
+        params.require(:quiz).permit(:name, :author)
+      end
+  
   end
