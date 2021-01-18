8e1d6e6860e46e600c8ca9b4861183ccf7c563f2
kirstenland <kirsten.land@softwire.com>
2021-01-18 19:16:39 +0000

    Reformat and start to add questions

--- db/schema.rb
+++ db/schema.rb
@@ -10,23 +10,42 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_16_221812) do
+ActiveRecord::Schema.define(version: 2021_01_18_103118) do
 
-  create_table 'quizzes', force: :cascade do |t|
-    t.string 'name'
-    t.string 'author'
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
+  create_table "connections", force: :cascade do |t|
+    t.string "link"
+    t.integer "question_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["question_id"], name: "index_connections_on_question_id"
   end
 
-  create_table 'rounds', force: :cascade do |t|
-    t.string 'type'
-    t.integer 'quiz_id', null: false
-    t.integer 'round_number'
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
-    t.index ['quiz_id'], name: 'index_rounds_on_quiz_id'
+  create_table "questions", force: :cascade do |t|
+    t.string "type"
+    t.string "symbol"
+    t.integer "round_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["round_id"], name: "index_questions_on_round_id"
   end
 
-  add_foreign_key 'rounds', 'quizzes'
+  create_table "quizzes", force: :cascade do |t|
+    t.string "name"
+    t.string "author"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "rounds", force: :cascade do |t|
+    t.string "type"
+    t.integer "quiz_id", null: false
+    t.integer "round_number"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["quiz_id"], name: "index_rounds_on_quiz_id"
+  end
+
+  add_foreign_key "connections", "questions"
+  add_foreign_key "questions", "rounds"
+  add_foreign_key "rounds", "quizzes"
 end
